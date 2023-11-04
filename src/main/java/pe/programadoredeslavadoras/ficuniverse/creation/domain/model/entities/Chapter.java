a2b6f5d0204130a3ebf624b3c18bbd8f6350f378
Stevens <u20201e735@upc.edu.pe>
2023-11-03 22:15:52 -0500

    feat: adding chapter entity annotations

--- src/main/java/pe/programadoredeslavadoras/ficuniverse/creation/domain/model/entities/Chapter.java
+++ src/main/java/pe/programadoredeslavadoras/ficuniverse/creation/domain/model/entities/Chapter.java
@@ -1,4 +1,23 @@
 package pe.programadoredeslavadoras.ficuniverse.creation.domain.model.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "chapters")
 public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name= "title", length = 30)
+    private String title;
+    @Column(name= "content")
+    private String content;
+
+
 }
