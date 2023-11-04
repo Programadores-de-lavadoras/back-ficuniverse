8fb9dd0e50ccaf5c8fcd0a89d9a2fd1b8021ebd3
Salomón Zegarra <salozm34@gmail.com>
2023-11-03 23:52:59 -0500

    refactor: added Repository annotation to Category Repository.

--- src/main/java/pe/programadoredeslavadoras/ficuniverse/creation/domain/persistance/CategoryRepository.java
+++ src/main/java/pe/programadoredeslavadoras/ficuniverse/creation/domain/persistance/CategoryRepository.java
@@ -1,10 +1,12 @@
 package pe.programadoredeslavadoras.ficuniverse.creation.domain.persistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pe.programadoredeslavadoras.ficuniverse.creation.domain.model.entities.Category;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 }
