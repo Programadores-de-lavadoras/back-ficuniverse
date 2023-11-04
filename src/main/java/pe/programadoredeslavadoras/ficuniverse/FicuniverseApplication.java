794a79fc5f2a87c1db8cecf8c5ceb8fbca711edf
Stevens <u20201e735@upc.edu.pe>
2023-11-03 20:37:11 -0500

    first commit: uploading project

---
+++ src/main/java/pe/programadoredeslavadoras/ficuniverse/FicuniverseApplication.java
@@ -0,0 +1,13 @@
+package pe.programadoredeslavadoras.ficuniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicuniverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicuniverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/programadoredeslavadoras/ficuniverse/FicuniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.programadoredeslavadoras.ficuniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicuniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
