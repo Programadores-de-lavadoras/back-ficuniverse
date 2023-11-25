e1a18f0b37c155f8079a539a104dc4aa783500e0
Salomón Zegarra <salozm34@gmail.com>
2023-11-25 03:27:31 -0500

    feat: added Profile Controller

--- src/main/java/pe/programadoredeslavadoras/ficuniverse/profile/domain/service/ProfileService.java
+++ src/main/java/pe/programadoredeslavadoras/ficuniverse/profile/domain/service/ProfileService.java
@@ -3,6 +3,6 @@ package pe.programadoredeslavadoras.ficuniverse.profile.domain.service;
 import pe.programadoredeslavadoras.ficuniverse.profile.domain.model.entities.Profile;
 
 public interface ProfileService {
-    public abstract Profile createProfile(Profile profile);
-    public abstract Profile getUserByEmail(String profile_email);
+    Profile save(Profile profile);
+    Profile findByEmail(String profile_email);
 }
